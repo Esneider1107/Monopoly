ff1046a7d59c2233479096cead0dfeda8d4d2dfa
FungiPr <samrosolar2007@gmail.com>
2025-11-02 10:04:13 -0500

    Add files via upload

---
+++ jugador.h
@@ -0,0 +1,27 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include <string>
+#include <vector>
+#include "tablero.h"
+
+struct Jugador{
+    std::string nombre;
+    float saldo;
+    int posicion;
+};
+
+Jugador crearJugador(std::string nombre);
+
+void mostrarInfoJugador(const Tablero& tablero, const Jugador& jugador);
+
+void AgregarDinero(Jugador& jugador, float monto);
+
+void RetirarDinero( Jugador& jugador, float monto);
+
+void ComprarPropiedad(Jugador& jugador,  Casilla& propiedad);
+
+void VenderPropiedad(Jugador& jugador, Casilla& propiedad);
+
+
+#endif
